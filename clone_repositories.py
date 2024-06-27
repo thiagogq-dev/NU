b7db276e515bcd5e2ac5f0a8b9690e4f819a0834
thiagogquinto <thiagogarianiq@gmail.com>
2024-06-27 18:55:32 -0300

    fix

--- clone_repositories.py
+++ clone_repositories.py
@@ -4,13 +4,18 @@ import sys
 
 json_file = sys.argv[1]
 
+if not os.path.exists("repos_dir"):
+    os.makedirs("repos_dir", exist_ok=True)
+os.chdir("repos_dir")
+
+if not os.path.exists(json_file):
+    print(f"File {json_file} does not exist")
+    sys.exit(1)
+
 with open(json_file) as f:
     data = json.load(f)
     for entry in data:
         repo_name = entry["repo_name"]
-        if not os.path.exists("pyszz_v2/repos_dir"):
-            os.makedirs("pyszz_v2/repos_dir", exist_ok=True)
 
-        os.chdir("pyszz_v2/repos_dir")
         print(f"Cloning {repo_name}")
         os.system(f"git clone  git clone https://github.com/{repo_name}.git {repo_name}")
