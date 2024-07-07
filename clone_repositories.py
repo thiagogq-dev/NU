0c0c7ed4be94bc0ffe8134630aee53b76e1ab1c1
thiagogquinto <thiagogarianiq@gmail.com>
2024-07-07 13:09:07 -0300

    update

--- clone_repositories.py
+++ clone_repositories.py
@@ -13,8 +13,9 @@ if not os.path.exists(json_file):
 
 with open(json_file) as f:
     data = json.load(f)
-    for entry in data:
-        repo_name = entry["repo_name"]
+    repo_name = data[0]["repo_name"]
+    # for entry in data:
+    #     repo_name = entry["repo_name"]
 
-        print(f"Cloning {repo_name}")
-        os.system(f"git clone https://github.com/{repo_name}.git pyszz_v2/repos_dir/{repo_name}")
+    #     print(f"Cloning {repo_name}")
+    os.system(f"git clone https://github.com/{repo_name}.git pyszz_v2/repos_dir/{repo_name}")
