2d3fed0b426dc905e3ce3f97c25248f42289a69f
thiagogquinto <thiagogarianiq@gmail.com>
2024-09-20 18:42:02 -0300

    finish NVD

--- split_by_repo_name.py
+++ split_by_repo_name.py
@@ -3,7 +3,7 @@ import os
 from collections import defaultdict
 
 # Carregar os dados do JSON
-with open('./hugo-selected-NONSECUR.json', 'r') as file:
+with open('./json/later/nvd/data_not_found.json', 'r') as file:
     data = json.load(file)
 
 # Agrupar os dados pelo campo 'repo_name'
